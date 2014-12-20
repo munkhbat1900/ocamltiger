21c163690fd1acc676cb6453375d359797eddff8
Avirmed Munkhbat <munkhbataa@gmail.com>
2014-12-20 16:47:17 +0900

    途中

--- parser/ast.ml
+++ parser/ast.ml
@@ -1,4 +1,4 @@
-type pos = int
+type pos = int and symbol = Symbol.symbol
 
 type var = SimpleVar of pos
 	   | FieldVar of var * pos
@@ -8,4 +8,10 @@ and exp = VarExp of var
 	  | NilExp
 	  | IntExp of int
 	  | StringExp of string * pos
-	  | CallExp of {func : }
+	  | CallExp of {func: symbol, args: exp list, pos: pos}
+	  | OpExp of {left: exp, oper: oper, right: exp, pos: pos}
+	  | RecordExp of {fields: (symbol * exp * pos) list, typ: symbol, pos: pos}
+	  | SeqExp of (exp * pos) list
+	  | AssignExp of (var: var, exp: exp, pos: pos)
+	  | IfExp of {test: exp, then': exp, else': exp option, pos: pos}
+	  | WhileExp
